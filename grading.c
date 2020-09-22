e197c92a084147882e75e52bec789e60a1f7461f
swathi-nuthalapati <71589081+swathi-nuthalapati@users.noreply.github.com>
2020-09-22 04:40:59 -0700

    created grading.c
    this file prints grade that which the particular marks enter by user

---
+++ grading.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main()
+{
+int marks;
+printf("Enter your marks:");
+scanf("%d",&marks);
+if(marks<0)
+{
+printf("wrong entry");
+}
+elseif(marks<40)
+{
+printf("Grade F");
+}
+elseif(marks>=54 && marks<69)
+{
+printf("Grade D");
+}
+elseif(marks>=69 && marks<84)
+{
+printf("Grade C");
+}
+elseif(marks>=84 && marks<100)
+{
+printf("Grade B");
+}
+elseif(marks<=100 && marks>=85)
+{
+printf("Grade A");
+}
+getch();
+}
